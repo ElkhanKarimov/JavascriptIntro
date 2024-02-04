2b42c60084a5a2b0fa74f4e5945976f5a5a1aed7
ElkhanKarimov <elxan0409@gmail.com>
2024-02-05 01:08:13 +0400

    task complated

---
+++ style.js
@@ -0,0 +1,111 @@
+// task1
+
+// let num = 7245
+
+// if (num>=1000 && num<10000) {
+//     console.log("7" + num + "8");
+// }
+// else{
+//     console.log("eded 4 reqemli deyil");
+// }
+
+
+
+// task 2
+
+
+// for (let i = 1; i < 1000; i++) {
+
+//    if (i%3===0) {
+//     console.log(i);
+//    }
+  
+// }
+
+
+// task 3
+
+// let num1 = 7777
+
+// let num2 = 9999999
+
+// let a = num1 * 0.04
+
+// let b = num2 * 0.09
+
+// let c = (a + b) * 0.1
+
+// console.log(c);
+
+
+
+// task 4
+
+// let qapien = 100
+// let qapihundurluk = 220
+
+// let skafen = 50
+// let skafuzunluq = 200
+// let skafhundurluk = 250
+
+// if ( skafen<=qapien && skafhundurluk<qapihundurluk || skafuzunluq<qapien && skafhundurluk<qapihundurluk || skafhundurluk<qapien && skafen<qapihundurluk) {
+//     console.log("kecer");
+// }
+// else{
+//     console.log("kecmez");
+// }
+
+
+
+// task 5
+
+// let num = 777
+
+// let a = num % 10
+
+// switch (a) {
+//     case 1:
+//         console.log("I gun");
+//         break;
+//     case 2:
+//         console.log("II gun");
+//         break;
+//     case 3:
+//         console.log("III gun");
+//         break;
+//     case 4:
+//         console.log("IV gun");
+//         break;
+//     case 5:
+//         console.log("V gun");
+//         break;
+//     case 6:
+//         console.log("VI gun");
+//         break;
+//     case 7:
+//         console.log("VII gun");
+//         break;
+
+
+//     default:
+//         console.log("reqem 7 den boyukdur");
+//         break;
+// }
+
+
+
+// task6
+
+// let nums = [24, 82, 55, 777, 0, 224];
+
+// let a = nums[0];
+
+// for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] > a) {
+//         a = nums[i];
+//     }
+// }
+
+// console.log(a);
+
+
